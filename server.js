533214a3f4272f90d23b7ac930cd8bea828ea5e9
chehockbeng <hockbeng.che@convoy.com.hk>
2023-08-21 17:27:58 +0800

    Code refactor

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ else {
     https.createServer({
         key: privateKey,
         cert: certificate
-    }, app).listen(port, () => { console.log('Server started on port ' + port) });
+    }, app).listen(port,() => {console.log('Server started on port ' + port);})
 }
 
 mongo.getConnection();
